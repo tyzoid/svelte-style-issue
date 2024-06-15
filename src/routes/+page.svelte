628ff287af14287304a1a701c66d92e957f4abe6
Tyler Dence <tyzoid.d@gmail.com>
2024-06-15 13:50:59 -0400

    Created repro

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<style>
+body { color: #fff; }
+</style>
+
+<a href="/">Home</a>
+<a href="/a">Test</a>
+<h1>Home (BG should be white)</h1>
---
+++ src/routes/a/+page.svelte
@@ -0,0 +1,3 @@
+<a href="/">Home</a>
+<a href="/a">Test</a>
+<h1>Test (BG should be red)</h1>
